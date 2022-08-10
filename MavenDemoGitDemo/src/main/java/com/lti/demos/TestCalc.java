a993e484aac2485757c9047f9cb94fc4c8e95d7f
Shivam <shivam297kush@gmail.com>
2022-08-10 12:24:18 +0530

    first commit

---
+++ MavenDemoGitDemo/src/main/java/com/lti/demos/TestCalc.java
@@ -0,0 +1,18 @@
+package com.lti.demos;
+
+public class TestCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculator c= new Calculator();
+		String msg= c.sayHello();
+
+		System.out.println(msg);
+		
+		int s= c.addNos(4, 798);
+		System.out.print(s);
+		
+	}
+
+}
